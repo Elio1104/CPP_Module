bdd2b15298c472889c06538b5c6cd365a34fc99c
alondot <alondot@student.s19.be>
2023-03-15 13:40:33 +0100

    ex_03

---
+++ cpp_01/ex_03/HumanA.cpp
@@ -0,0 +1,17 @@
+#include "HumanA.hpp"
+
+HumanA::HumanA(std::string name, Weapon& weapon) : _name(name) , _weapon(weapon)
+{
+}
+
+HumanA::~HumanA()
+{
+}
+
+void HumanA::attack()
+{
+	if (this->_weapon.getType() != "")
+		std::cout << _name << " attacks with their " << this->_weapon.getType() << std::endl;
+	else
+		std::cout << _name << " doesn't have a weapon." << std::endl;
+}
---
+++ cpp_01/ex_03/HumanB.hpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "Weapon.hpp"
+
+class HumanB
+{
+private:
+	std::string _name;
+	Weapon* _weapon;
+
+public:
+	HumanB(std::string name);
+	~HumanB();
+
+	void attack();
+	void setWeapon(Weapon& weapon);
+};
