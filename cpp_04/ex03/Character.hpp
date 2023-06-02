6d1f1f72832f645a58df77b97dc773bdc236d102
alondot <alondot@student.s19.be>
2023-06-02 15:54:01 +0200

    ajout du bonus

---
+++ cpp_04/ex03/Character.hpp
@@ -0,0 +1,27 @@
+#ifndef CHARACTER_HPP
+#define CHARACTER_HPP
+
+#include <iostream>
+#include "ICharacter.hpp"
+#include "AMateria.hpp"
+
+class Character : public ICharacter
+{
+private:
+    std::string _name;
+    AMateria    *_inventory[4];
+
+public:
+    Character(std::string const &name);
+    Character(Character const &other);
+    ~Character();
+
+    Character& operator=(Character const &other);
+
+    std::string const&  getName() const;
+    void equip(AMateria* m);
+    void unequip(int idx);
+    void use(int idx, ICharacter& target);
+};
+
+#endif
