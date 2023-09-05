78eff382a2d1411f3629327a61304479f750b8b4
alondot <alondot@student.s19.be>
2023-09-05 15:55:48 +0200

    update main value

--- cpp_06/ex_00/main.cpp
+++ cpp_06/ex_00/main.cpp
@@ -1,8 +1,19 @@
 #include "ScalarConverter.hpp"
 
+char ScalarConverter::_c;
+int ScalarConverter::_n;
+float ScalarConverter::_f;
+double ScalarConverter::_d;
+
+e_type ScalarConverter::_type;
+std::string ScalarConverter::_str;
+
+bool ScalarConverter::_impossible;
+
 int main (int ac, char **av) {
     if ( ac != 2 ) {
         std::cout << "Usage : ./convert <number>" << std::endl;
+        return 0;
     }
     try
     {
