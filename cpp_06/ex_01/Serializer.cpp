b4ae2199d578ff7bd5c33e78421dd713104c7569
alondot <alondot@student.s19.be>
2023-09-05 15:45:45 +0200

    update for mac

--- cpp_06/ex_01/Serializer.cpp
+++ cpp_06/ex_01/Serializer.cpp
@@ -11,10 +11,10 @@ Serializer& Serializer::operator=( const Serializer& other ) {
     return *this ;
 }
 
-uintptr_t serialize(Data* ptr) {
+uintptr_t Serializer::serialize(Data* ptr) {
     return ( reinterpret_cast< uintptr_t >( ptr ) );
 }
 
-Data* deserialize(uintptr_t raw) {
+Data* Serializer::deserialize(uintptr_t raw) {
     return ( reinterpret_cast< Data* >( raw ) );
 }
